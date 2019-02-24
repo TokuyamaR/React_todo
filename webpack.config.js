ded6679c0ff80ba1077deb287f17e913c74ac77c
徳山 来人 <tokuyamaraido@team-lab.com>
2019-02-24 13:23:18 +0900

    190224 first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack(モジュールを束ねるツール)に関する設定ファイル
+// webpackとは：https://goworkship.com/magazine/how-to-webpack/
+// webpack.config.jsに関する設定方法：https://original-game.com/how-to-use-webpack-config-js/
+
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/js/app.js'), // プログラムの実行を開始するファイル
+    output: { // 出力先ファイルの設定
+        path: path.join(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, //変換対象ファイルの設定
+                exclude: /node_module/, // 対象外とするファイルの設定
+                loader: 'babel-loader', // loaderの設定
+                query: {
+                    presets: ['react', 'env']
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.join(__dirname, 'src'), 'node_module'],
+        extensions: ['.js', '.jsx']
+    }
+};
